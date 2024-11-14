d70a9831dcb715acfd71645abad3d7a1170d0e07
Daniel McClean <dmcclean780@gmail.com>
2024-11-14 12:51:11 +0000

    moved SubMenu super before other property assignment

--- src/part01/SubMenu.java
+++ src/part01/SubMenu.java
@@ -9,13 +9,13 @@ public class SubMenu extends Menu {
      * @param type - the item the sub menu is for - artifacts or exhibits
      */
     public SubMenu(String type){
-        this.type=type;
         String menuTitle = "Manage "+type+"s";
         String[] menuOptions = {"Add ", "View ", "Update ", "Delete ", "Quit"};
         for (int i=0; i<menuOptions.length-1; i++){
             menuOptions[i]+=type;
         }
         super(menuTitle, menuOptions);
+        this.type=type;
     }
 
     /**
